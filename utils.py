3e316888c7db7ddda79b3d0df0d8c313c85624d7
Drew Gustafson <drewgustafson12@gmail.com>
2025-07-13 12:55:58 -0700

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,32 @@
+import datetime
+import numpy as np
+
+def process_odds(odds):
+    odds = odds.replace(' ', '')
+    sign = None
+    for char in odds:
+        if char in ('+', '-'):
+            sign = char
+            break
+    if not sign:
+        raise ValueError("Invalid odds format")
+    num = float(odds.replace(sign, ''))
+    return sign, num
+
+def get_date(date_str):
+    if date_str.lower() == 't':
+        date = datetime.date.today()
+    else:
+        date = datetime.datetime.strptime(date_str, '%Y-%m-%d').date()
+    return date
+
+def process_results(s):
+    r = s.upper()
+    if r not in ['W', 'L', 'P']:
+        r = np.nan
+    return r
+
+def pct(your_number, num_digits=2):
+    s = "{:.num_digits%}"
+    s = s.replace('num_digits', str(num_digits))
+    return s.format(your_number)
